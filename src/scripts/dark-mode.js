db15b913f7edd65ed5e290a7534ca0e4cd078824
Luis Landkoer <llandkoer@gmail.com>
2021-03-23 17:33:46 -0500

    add dark mode

---
+++ src/scripts/dark-mode.js
@@ -0,0 +1,52 @@
+const btnSwitch = document.querySelector("#switch");
+const logo = document.querySelector("#logo");
+const camera = document.querySelector("#camara");
+const movie = document.querySelector("#movie");
+
+btnSwitch.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark") === true) {
+    btnSwitch.innerHTML = "Modo Diurno";
+
+    logo.removeAttribute("src");
+    logo.setAttribute("src", "../src/assets/logo-dark-mode.svg");
+
+    camera.removeAttribute("src");
+    camera.setAttribute("src", "../src/assets/illustration-camara-dark.svg");
+
+    movie.removeAttribute("src");
+    movie.setAttribute("src", "../src/assets/Illustration-movie-dark.svg");
+
+    localStorage.setItem('dark-mode', 'true')
+  } else {
+    btnSwitch.innerHTML = "Modo Nocturno";
+    logo.removeAttribute("src");
+    logo.setAttribute("src", "../src/assets/logo.svg");
+
+    camera.removeAttribute("src");
+    camera.setAttribute("src", "../src/assets/illustration-camara.svg");
+
+    movie.removeAttribute("src");
+    movie.setAttribute("src", "../src/assets/illustration-movie.svg");
+
+    localStorage.setItem('dark-mode', 'false')
+  }
+});
+
+if (localStorage.getItem('dark-mode') === 'true'){
+  document.body.classList.add("dark");
+
+  btnSwitch.innerHTML = "Modo Diurno";
+
+  logo.removeAttribute("src");
+    logo.setAttribute("src", "../src/assets/logo-dark-mode.svg");
+
+  camera.removeAttribute("src");
+    camera.setAttribute("src", "../src/assets/illustration-camara-dark.svg");
+
+    movie.removeAttribute("src");
+    movie.setAttribute("src", "../src/assets/Illustration-movie-dark.svg");
+} else{
+  document.body.classList.remove("dark");
+}
